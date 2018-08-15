3b3037f7f1bab402fe3c75cd20ab18e04e1cbe0b
lmgonzalves <luismgonzalves@gmail.com>
2018-08-15 13:38:40 -0400

    Update Portfolio Carousel demo

--- js/portfolio-carousel.js
+++ js/portfolio-carousel.js
@@ -1,8 +1,10 @@
 (function() {
 
+    var slidersContainer = document.querySelector('.sliders-container');
+
     // Initializing the numbers slider
     var msNumbers = new MomentumSlider({
-        el: '.ms--numbers-container',
+        el: slidersContainer,
         cssClass: 'ms--numbers',
         range: [1, 4],
         rangeContent: function (i) {
@@ -16,8 +18,19 @@
     });
 
     // Initializing the titles slider
+    var titles = [
+        'King of the Ring Fight',
+        'Sound of Streets',
+        'Urban Fashion',
+        'Windy Sunset'
+    ];
     var msTitles = new MomentumSlider({
-        el: '.ms--titles',
+        el: slidersContainer,
+        cssClass: 'ms--titles',
+        range: [0, 3],
+        rangeContent: function (i) {
+            return '<h3>'+ titles[i] +'</h3>';
+        },
         vertical: true,
         reverse: true,
         style: {
@@ -28,7 +41,12 @@
 
     // Initializing the links slider
     var msLinks = new MomentumSlider({
-        el: '.ms--links',
+        el: slidersContainer,
+        cssClass: 'ms--links',
+        range: [0, 3],
+        rangeContent: function () {
+            return '<a class="ms-slide__link">View Case</a>';
+        },
         vertical: true,
         interactive: false
     });
@@ -39,13 +57,24 @@
 
     // Initializing the images slider
     var msImages = new MomentumSlider({
-        el: '.ms--images',
+        // Element to append the slider
+        el: slidersContainer,
+        // CSS class to reference the slider
+        cssClass: 'ms--images',
+        // Generate the 4 slides required
+        range: [0, 3],
+        rangeContent: function () {
+            return '<div class="ms-slide__image-container"><div class="ms-slide__image"></div></div>';
+        },
+        // Syncronize the other sliders
         sync: [msNumbers, msTitles, msLinks],
+        // Styles to interpolate as we move the slider
         style: {
             '.ms-slide__image': {
                 transform: [{scale: [1.5, 1]}]
             }
         },
+        // Update pagination if slider change
         change: function(newIndex, oldIndex) {
             if (typeof oldIndex !== 'undefined') {
                 paginationItems[oldIndex].classList.remove('pagination__item--active');
@@ -54,6 +83,7 @@
         }
     });
 
+    // Select corresponding slider item when a pagination button is clicked
     pagination.addEventListener('click', function(e) {
         if (e.target.matches('.pagination__button')) {
             var index = paginationItems.indexOf(e.target.parentNode);
